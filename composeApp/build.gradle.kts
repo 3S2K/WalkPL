3d675328761dab971a234b82b5d251998bc8dfcf
haesol1013 <shinhaesol11@gmail.com>
2024-12-06 07:18:23 +0900

    refactor: Reorganize dependencies management in build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -77,6 +77,9 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.json)
+            
+            // Serialization
+            implementation(libs.kotlinx.serialization.json)
         }
 
         androidMain.dependencies {
